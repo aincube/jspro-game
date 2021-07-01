a4fdffaa5d08a8f6ef64fad40124ebdb397b0650
aincube <aincube@gmail.com>
2021-07-02 01:00:19 +0200

    First 3 tasks DONE

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import HeroSprite from './assets/Male-2-Walk.png';
 
 const canvas = document.getElementById('game');
 const ctx = canvas.getContext('2d');
+const fieldW = 600;
+const fieldH = 600;
 const spriteW = 48;
 const spriteH = 48;
+const lastXPos = fieldW - spriteW;
+const lastYPos = fieldH - spriteH;
 const shots = 3;
 let cycle = 0;
-// let pY = 0;
+let pX = fieldW / 2 - spriteW / 2;
+let pY = fieldH / 2 - spriteH / 2;
+let spriteShift = 0;
 let DownPressed = false;
 let UpPressed = false;
 let LeftPressed = false;
@@ -53,10 +59,30 @@ document.addEventListener('keyup', (e) => {
   keytrigger(e, false);
 });
 
+/* eslint no-unused-expressions: [2, { allowTernary: true }] */
 img.addEventListener('load', () => {
   setInterval(() => {
-    cycle = (cycle + 1) % shots;
-    ctx.clearRect(0, 0, 600, 600);
-    ctx.drawImage(img, cycle * spriteW, 48, spriteW, spriteH, 0, 0, 48, 48);
+    if (DownPressed) {
+      spriteShift = 0;
+      pY < lastYPos ? pY += 10 : pY = lastYPos;
+      cycle = (cycle + 1) % shots;
+    }
+    if (UpPressed) {
+      spriteShift = 144;
+      pY > 0 ? pY -= 10 : pY = 0;
+      cycle = (cycle + 1) % shots;
+    }
+    if (LeftPressed) {
+      spriteShift = 48;
+      pX > 0 ? pX -= 10 : pX = 0;
+      cycle = (cycle + 1) % shots;
+    }
+    if (RightPressed) {
+      spriteShift = 96;
+      pX < lastXPos ? pX += 10 : pX = lastXPos;
+      cycle = (cycle + 1) % shots;
+    }
+    ctx.clearRect(0, 0, fieldW, fieldH);
+    ctx.drawImage(img, cycle * spriteW, spriteShift, spriteW, spriteH, pX, pY, spriteW, spriteH);
   }, 120);
 });
